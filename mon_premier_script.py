fa85c237e367250fa72e1912911acd8b667e7510
KevinPierronne <157361538+KevinPierronne@users.noreply.github.com>
2024-02-05 14:07:46 +0000

    feat: Clean Code Exercice KPIERRONNE

--- mon_premier_script.py
+++ mon_premier_script.py
@@ -1,15 +1,24 @@
+import unittest
+
 """
 Count names with more than seven letters
 """
-def names(prenoms):
+def compter_longueur_mot(prenoms : str) -> str:
+    limit_lettre = 7
     more_than_seven = 0
     for prenom in prenoms:
-        if len(prenom) > 7:
+        if len(prenom) > limit_lettre :
             more_than_seven += 1
             print("Prenom supérieur à 7 : " + prenom)
         else:
             print("Prenom inférieur ou égal à 7 : " + prenom)
     return more_than_seven
 
-prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-print("Nombre de prénoms supérieurs à 7 : " + str(names(prenoms=prenoms)))
+class TestNamesMethod(unittest.TestCase):
+     def test_compter_longueur_mot(self):
+        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        more_than_seven = compter_longueur_mot(prenoms=prenoms)
+        self.assertEqual(more_than_seven, 4)
+
+if __name__ == '__main__':
+    unittest.main()
